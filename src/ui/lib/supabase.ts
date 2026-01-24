67a6711a3d3777ee08be3e1b683e47ef2d07c929
Shahid Khan <shahid.khannn66@gmail.com>
2026-01-24 16:55:27 +0500

    [REFACTOR] setup supabase authProvider/AuthContext

---
+++ src/ui/lib/supabase.ts
@@ -0,0 +1,11 @@
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey: string = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase URL or Key');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
