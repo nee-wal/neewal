35b49c98a153fd9103641a03ef9b6242d8fbace7
Shahid Khan <shahid.khannn66@gmail.com>
2026-01-24 16:55:04 +0500

    [REFACTOR] make code dry

--- src/ui/components/Button.tsx
+++ src/ui/components/Button.tsx
@@ -1,11 +1,6 @@
 
 import React, { forwardRef } from 'react';
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
+import {cn} from "../lib/utils.ts";
 
 export interface ButtonProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement> {
--- src/ui/components/Input.tsx
+++ src/ui/components/Input.tsx
@@ -1,11 +1,6 @@
 
 import React, { forwardRef } from 'react';
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
+import {cn} from "../lib/utils.ts";
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
 
