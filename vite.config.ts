9a7e2772ad71bb11c337f047e66f933fe363cf9b
Shahid Khan <shahid.khannn66@gmail.com>
2026-01-24 15:41:46 +0500

    [REFACTOR] improve development experience

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      tailwindcss()
-  ],
+    plugins: [
+        react(),
+        tailwindcss()
+    ],
+    base: './',
+    build: {
+        outDir: './dist-react',
+    },
+    server: {
+        port: 5123,
+        strictPort: true
+    }
 })
